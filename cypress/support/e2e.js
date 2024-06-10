4496e0eb81c3f9e174313f52cf19bc1a9201ac85
nicolas-ferreira <nicolas@klever.io>
2024-06-10 20:27:49 +0100

    Added cypress setup

---
+++ cypress/support/e2e.js
@@ -0,0 +1,8 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// External libs
+import 'allure-cypress/commands'
+import 'cypress-plugin-api'
+import 'allure-cypress'
+import '@faker-js/faker'
