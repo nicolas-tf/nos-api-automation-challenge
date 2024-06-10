1032c3cc11c4eceda792c659a5d85ef1caf2e627
nicolas-ferreira <nicolas@klever.io>
2024-06-10 20:33:02 +0100

    Added health check test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter")
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://gorest.co.in/public/',
+    watchForFileChanges: false,
+    
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on)
+      return config;
     },
   },
-});
+
+  env: {
+    version: 'v2/',
+    hideCredentials: false
+  }
+
+})
